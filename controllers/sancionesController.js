ed5218ddb431e666a6eb03a0c919505cc2e5970e
Karlos-Daniel <karlosrodriguez28@gmail.com>
2025-11-15 14:32:31 -0500

    sancionesPost update

--- controllers/sancionesController.js
+++ controllers/sancionesController.js
@@ -3,6 +3,48 @@ const { Sanciones } = require('../models');
 const {validationResult} = require('express-validator');
 const moment = require('moment')
 
+// const sancionesPost = async (req = request, res = response) => {
+//     try {
+//         const {
+//             ID_alumno,
+//             ID_autoridad,
+//             ID_tipo_sancion,
+//             ID_duracion_sancion,
+//             fecha,
+//         } = req.body
+
+//         if(!isValidObjectId(ID_alumno && ID_autoridad && ID_tipo_sancion && ID_duracion_sancion )){
+//                     return res.status(400).json({errores:[{
+//                         msg:`este id: ${_id} no es de mongo`
+//                     }]})
+//                 }
+       
+//         const dateMoment = new Date(fecha)
+//         const fechaMoment = moment(dateMoment).format('MM/DD/YYYY');    
+           
+//         const data = {
+//            ID_alumno,
+//             ID_autoridad,
+//             ID_tipo_sancion,
+//             ID_duracion_sancion,
+//             fecha,
+//         }
+
+//         const sanciones = new Sanciones(data)
+
+//         await sanciones.save()
+
+//         return res.status(201).json({
+//             msg:'Sancion creada con exito'
+//         })
+
+//     } catch (error) {
+//         console.log(error);
+//         return res.status(500).json({
+//             msg: error
+//         })
+//     }
+// }
 const sancionesPost = async (req = request, res = response) => {
     try {
         const {
@@ -11,40 +53,49 @@ const sancionesPost = async (req = request, res = response) => {
             ID_tipo_sancion,
             ID_duracion_sancion,
             fecha,
-        } = req.body
+        } = req.body;
 
-        if(!isValidObjectId(ID_alumno && ID_autoridad && ID_tipo_sancion && ID_duracion_sancion )){
-                    return res.status(400).json({errores:[{
-                        msg:`este id: ${_id} no es de mongo`
-                    }]})
-                }
-       
-        const dateMoment = new Date(fecha)
-        const fechaMoment = moment(dateMoment).format('MM/DD/YYYY');    
-           
+        // Validar IDs individualmente
+        if (
+            !isValidObjectId(ID_alumno) ||
+            !isValidObjectId(ID_autoridad) ||
+            !isValidObjectId(ID_tipo_sancion) ||
+            !isValidObjectId(ID_duracion_sancion)
+        ) {
+            return res.status(400).json({
+                errores: [{
+                    msg: `Uno o más IDs no son válidos`
+                }]
+            });
+        }
+
+        // Convertir fecha correctamente
+        const fechaMoment = moment(fecha).toDate();
+
         const data = {
-           ID_alumno,
+            ID_alumno,
             ID_autoridad,
             ID_tipo_sancion,
             ID_duracion_sancion,
-            fecha,
-        }
+            fecha: fechaMoment,   // ← Ahora sí se guarda bien en Mongo
+        };
 
-        const sanciones = new Sanciones(data)
-
-        await sanciones.save()
+        const sancion = new Sanciones(data);
+        await sancion.save();
 
         return res.status(201).json({
-            msg:'Sancion creada con exito'
-        })
+            msg: 'Sanción creada con éxito',
+            sancion
+        });
 
     } catch (error) {
         console.log(error);
         return res.status(500).json({
-            msg: error
-        })
+            msg: 'Error en el servidor',
+        });
     }
-}
+};
+
 
 //editar
 const sancionesPut = async (req = request, res = response) => {
